8fd433d55ec74213c7468be469859008a061a566
ZillCo <166273667+ZillCo@users.noreply.github.com>
2025-02-17 17:32:05 +0800

    script.js

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
             status.textContent = "🔍 Searching...";
 
             // Make sure to use the correct API URL (either local or online)
-            const response = await fetch(`https://MAPS-API.onrender.com/geocode?address=${encodeURIComponent(searchBox)}`);
+            const response = await fetch(`https://https://maps-api-zhqt.onrender.com/geocode?address=${encodeURIComponent(searchBox)}`);
             const data = await response.json();
 
             if (data.error) {
