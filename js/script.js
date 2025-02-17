68a65c65e708ee6fedef9cc5ff1cba951470d0dc
ZillCo <166273667+ZillCo@users.noreply.github.com>
2025-02-17 17:25:45 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
             status.textContent = "🔍 Searching...";
 
             // Make sure to use the correct API URL (either local or online)
-            const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(searchBox)}`);
+            const response = await fetch(`https://MAPS-API.onrender.com/geocode?address=${encodeURIComponent(searchBox)}`);
             const data = await response.json();
 
             if (data.error) {
